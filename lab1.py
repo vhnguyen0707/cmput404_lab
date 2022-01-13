57067a43235759422fd0c593f6b54321b77d5401
vhnguyen0707 <nhvu@ualberta.ca>
2022-01-12 20:12:20 -0700

    Fix minor bug

--- lab1.py
+++ lab1.py
@@ -1,9 +1,9 @@
 import requests
-import os
 
 # step 4 of virtualenv
 print(requests.__version__)
 
 # step 5 of curl
 response = requests.get('http://google.com/')
-print(response.json())
+print(response)
+
